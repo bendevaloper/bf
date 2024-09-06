db0f24a8a989bae2f6934e9f51968949a1ea4fad
Guvonlive <142746767+bendevaloper@users.noreply.github.com>
2024-09-06 23:40:44 +0700

    //

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    BASE_URL: "https://axtro.risuvo.com/", // You must have a base API URL
+  },
+};
+
+export default nextConfig;
